2b436c7e754ebad2cfb92fc667298184106a3ffe
Sourav <ssjsaha@gmail.com>
2024-01-28 14:25:02 -0600

    TwitterLite-0067: Added logout button

--- app/src/main/java/com/example/twitterlite/presentation/ui/HomePage.kt
+++ app/src/main/java/com/example/twitterlite/presentation/ui/HomePage.kt
@@ -1,43 +1,38 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.twitterlite.presentation.ui
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.navigation.NavController
 import com.example.twitterlite.BuildConfig
-import com.example.twitterlite.domain.model.Post
 import com.example.twitterlite.presentation.ui.model.PostUI
 import kotlinx.coroutines.flow.StateFlow
 import java.util.Objects
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(
     state: StateFlow<HomeState>,
@@ -64,17 +59,31 @@ fun HomePage(
         }
     }
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Twitter Lite") },
+                actions = {
+                    Text(text = "Logout", Modifier.clickable {
+                        navController.navigate("auth") {
+                            popUpTo("home") {
+                                inclusive = true
+                            }
+                        }
+                    })
+                }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = {
                 navController.navigate("post")
-         /*       val permissionCheckResult =
-                    ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
-                if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
-                    cameraLauncher.launch(uri)
-                } else {
-                    // Request a permission
-                    permissionLauncher.launch(Manifest.permission.CAMERA)
-                }*/
+                /*       val permissionCheckResult =
+                           ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                       if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                           cameraLauncher.launch(uri)
+                       } else {
+                           // Request a permission
+                           permissionLauncher.launch(Manifest.permission.CAMERA)
+                       }*/
             }) {
                 Icon(
                     Icons.Default.Add,
